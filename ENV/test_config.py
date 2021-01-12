fff0228ad813d5d799a4a92ddd04ebdb22c0229d
Nandy Bâ <nandyba@yahoo.fr>
2021-01-13 00:56:35 +0100

    Ajout d'un test unitaire pour vérifier la presence de l'API Secret Binance dans les variables d'environement

--- ENV/test_config.py
+++ ENV/test_config.py
@@ -8,5 +8,10 @@ class TestConfig(unittest.TestCase):
         self.assertIsNotNone(key)
         self.assertIsNot(key, "")
 
+    def test_check_presence_of_Binance_API_Secret(self):
+        key = os.getenv('Binance_API_Secret')
+        self.assertIsNotNone(key)
+        self.assertIsNot(key, "")
+
 if __name__ == '__main__':
     unittest.main()
