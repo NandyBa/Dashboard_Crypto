cac07494f46b10e5549e908a70e38bc4a15304e8
Nandy Bâ <nandyba@yahoo.fr>
2021-01-13 16:02:06 +0100

    Implémentation de la méthode BinanceExchange.get_balances

--- Binance.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.append("ENV")
-import config
-
-class Binance():
-	pass
---
+++ BinanceExchange.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.append("ENV")
+import config
+from binance.client import Client
+
+class BinanceExchange():
+
+	def __init__(self):
+		self.CLIENT = Client(os.getenv('Binance_API_Key'), os.getenv('Binance_API_Secret'), {"verify": True, "timeout": 20})
+
+	def get_balances(self):
+		balances = []
+		info = self.CLIENT.get_account()
+		for asset in info['balances']:
+			if float(asset['free']) != 0 or float(asset['locked']) != 0:
+				balances.append(asset)
+		return balances
