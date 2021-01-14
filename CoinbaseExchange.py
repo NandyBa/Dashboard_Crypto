d9c33c152eee1c895ca7ee999e2ab0817eef5b25
Nandy Bâ <nandyba@yahoo.fr>
2021-01-14 19:39:03 +0100

    Implémentation de la méthode CoinbaseExchange.get_balances

--- CoinbaseExchange.py
+++ CoinbaseExchange.py
@@ -3,5 +3,21 @@ import sys
 sys.path.append("ENV")
 import config
 
+from coinbase.wallet.client import Client
+
 class CoinbaseExchange():
-	pass
+
+	def __init__(self):
+		self.CLIENT = Client(os.environ["Coinbase_API_Key"], os.environ["Coinbase_API_Secret"])
+
+	def get_balances(self):
+		balances = []
+		info = self.CLIENT.get_accounts()
+		for asset in info['data']:
+			if float(asset['balance']['amount']) != 0:
+				balance = {
+					"amount": float(asset['balance']['amount']),
+					"currency":asset['balance']['currency']
+				}
+				balances.append(balance)
+		return balances
