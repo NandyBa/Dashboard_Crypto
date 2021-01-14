c6ac276ac827fdf123699daef3defac8e95d828c
Nandy Bâ <nandyba@yahoo.fr>
2021-01-15 00:22:20 +0100

    Implémentation d'une méthode de la classe CSVCompiler répondant au test Coinbase_compile

--- CSVCompiler.py
+++ CSVCompiler.py
@@ -1,2 +1,16 @@
+import pandas as pd
+import CoinbaseExchange
+
 class CSVCompiler():
-    pass
+    def compilecsv(self, exchanges = [], file = "summary_crypto.csv"):
+        Coinbase = pd.Series(dtype='int', name="Coinbase")
+        if("Coinbase" in exchanges):
+            c = CoinbaseExchange.CoinbaseExchange()
+            CoinbaseBalances  = c.get_balances()
+            
+            for balance in CoinbaseBalances:
+                Coinbase[balance["currency"]] = balance["amount"]
+        
+        
+        df = pd.DataFrame({ "Coinbase": Coinbase})
+        df.to_csv(file,sep=';',index=True)
