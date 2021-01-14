e8de58c6c974df2bfc2517d6eb833a0ab44dc78f
Nandy Bâ <nandyba@yahoo.fr>
2021-01-14 18:47:29 +0100

    Ajout des variables d'environement concernant l'API Coinbase dans le fichier de configuration

--- ENV/config.py
+++ ENV/config.py
@@ -2,4 +2,6 @@ import os
 
 # Set environement variables
 os.environ["Binance_API_Key"] = ""
-os.environ["Binance_API_Secret"] = "" 
+os.environ["Binance_API_Secret"] = ""
+os.environ["Coinbase_API_Key"] = ""
+os.environ["Coinbase_API_Secret"] = "" 
