89a5846a6fef2007cf877a9da7d53adbb8797214
Nandy Bâ <nandyba@yahoo.fr>
2021-01-15 12:29:49 +0100

    Implémentation d'une méthode de la classe CSVCompiler répondant au test Binance_compile (Spot & Flexible Saving)

--- CSVCompiler.py
+++ CSVCompiler.py
@@ -1,16 +1,28 @@
 import pandas as pd
 import CoinbaseExchange
+import BinanceExchange
 
 class CSVCompiler():
     def compilecsv(self, exchanges = [], file = "summary_crypto.csv"):
         Coinbase = pd.Series(dtype='int', name="Coinbase")
+        Binance = pd.Series(dtype='int', name="Binance")
+        BinanceFlexibleSaving = pd.Series(dtype='int', name="BinanceEarn")
         if("Coinbase" in exchanges):
             c = CoinbaseExchange.CoinbaseExchange()
             CoinbaseBalances  = c.get_balances()
             
             for balance in CoinbaseBalances:
                 Coinbase[balance["currency"]] = balance["amount"]
+        if("Binance" in exchanges):
+            c = BinanceExchange.BinanceExchange()
+            BinanceBalances  = c.get_balances()
+            
+            for balance in BinanceBalances:
+                if "LD" in balance["asset"][0:2]:
+                    BinanceFlexibleSaving[balance["asset"].replace("LD","")] = balance["free"] + balance["locked"]
+                else:
+                    Binance[balance["asset"]] = balance["free"] + balance["locked"]
         
         
-        df = pd.DataFrame({ "Coinbase": Coinbase})
+        df = pd.DataFrame({ "Coinbase": Coinbase,"Binance": Binance, "Binance Flexible Saving": BinanceFlexibleSaving})
         df.to_csv(file,sep=';',index=True)
