064d421c1481a18f8fac478436171569c67ae436
Nandy Bâ <nandyba@yahoo.fr>
2021-01-17 02:40:36 +0100

    Netoyage du code

--- test_CoinbaseExchange.py
+++ test_CoinbaseExchange.py
@@ -11,40 +11,40 @@ importlib.reload(CoinbaseExchange)
 
 class TestConfig(TestCase):
 
-    @mock.patch('coinbase.wallet.client.Client.get_accounts', return_value = {"data": [                                                                               
-        {                                                                                     
-          "allow_deposits": True,                                                             
-          "allow_withdrawals": True,                                                          
-          "balance": {                                                                        
-            "amount": "2.567235",                                                          
-            "currency": "GRT"                                                                 
-          },                                                                                                                                                                                  
-          "native_balance": {                                                                 
-            "amount": "6.25",                                                                 
-            "currency": "EUR"                                                                 
-          },                                                                                                                               
-        },                                                                                    
-        {                                                                                     
-          "allow_deposits": True,                                                             
-          "allow_withdrawals": True,                                                          
-          "balance": {                                                                        
-            "amount": "0.00000000",                                                           
-            "currency": "BAND"                                                                
-          },                                                                                                                                                                            
-          "native_balance": {                                                                 
-            "amount": "0.00",                                                                 
-            "currency": "EUR"                                                                 
-          },                                                                                                                               
-        },                                                                                    
-        {                                                                                                                                             
-          "balance": {                                                                        
-            "amount": "0.175864",                                                            
-            "currency": "ALGO"                                                                
-          },                                                                                                                                                                       
-          "native_balance": {                                                                 
-            "amount": "0.85",                                                                 
-            "currency": "EUR"                                                                 
-          },                                                                                                                                  
+    @mock.patch('coinbase.wallet.client.Client.get_accounts', return_value = {"data": [
+        {
+          "allow_deposits": True,
+          "allow_withdrawals": True,
+          "balance": {
+            "amount": "2.567235",
+            "currency": "GRT"
+          },
+          "native_balance": {
+            "amount": "6.25",
+            "currency": "EUR"
+          },
+        },
+        {
+          "allow_deposits": True,
+          "allow_withdrawals": True,
+          "balance": {
+            "amount": "0.00000000",
+            "currency": "BAND"
+          },
+          "native_balance": {
+            "amount": "0.00",
+            "currency": "EUR"
+          },
+        },
+        {
+          "balance": {
+            "amount": "0.175864",
+            "currency": "ALGO"
+          },
+          "native_balance": {
+            "amount": "0.85",
+            "currency": "EUR"
+          },
         }
     ]})
     def test_get_balances(self, mock_get_account):
