bc0b0d23f1e17fb2b1485cee7b0b994c1d23b6db
Nandy Bâ <nandyba@yahoo.fr>
2021-01-18 02:40:44 +0100

    Implémentation de la méthode permetant de générer le Bilan des finances

--- CSVCompiler.py
+++ CSVCompiler.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import CoinbaseExchange
 import BinanceExchange
+import Exchanges
 import xlsxwriter
+import openpyxl
 
 class CSVCompiler():
     def compilecsv(self, exchanges = [], file = "summary_crypto.xlsx"):
@@ -29,3 +31,49 @@ class CSVCompiler():
         writer = pd.ExcelWriter(file, engine='xlsxwriter')
         df.to_excel(writer, sheet_name="Balances")
         writer.save()
+
+    def compile_financial_statement(self, file = "summary_crypto.xlsx"):
+        wb_obj = openpyxl.load_workbook(file)
+        sheetToRead = wb_obj['Balances']
+        ex = Exchanges.Exchanges()
+
+        financial_statement_sheet = wb_obj.create_sheet(title="Financial Statement")
+
+        Prices = pd.Series(dtype='float', name="Price")
+        Valorisations = pd.Series(dtype='int', name="Valorisation")
+
+        for row in sheetToRead.iter_rows():
+            if row[0].value != None :
+                asset = row[0].value
+                Prices[asset] = ex.get_Price(asset,"EUR")
+                sum_ = 0
+                for i in range(1,4):
+                    if row[i].value == None:
+                        sum_ += 0
+                    else:
+                        sum_ += row[i].value
+
+                Valorisations[asset] = Prices[asset] * sum_
+
+        _ = financial_statement_sheet.cell(column=2, row=1, value="Abreviation")
+        _ = financial_statement_sheet.cell(column=3, row=1, value="EUR/U")
+        _ = financial_statement_sheet.cell(column=4, row=1, value="")
+        for i in range(2,len(Prices)+2):
+            financial_statement_sheet.cell(column=1, row=i, value=Valorisations[i-2]/sum(Valorisations)).number_format = '0.00%'
+            _ = financial_statement_sheet.cell(column=2, row=i, value=Prices.index[i-2])
+            _ = financial_statement_sheet.cell(column=3, row=i, value=Prices[i-2])
+            _ = financial_statement_sheet.cell(column=4, row=i, value=Valorisations[i-2])
+
+
+        _ = financial_statement_sheet.cell(column=1, row=len(Prices)+3, value="Total:")
+        _ = financial_statement_sheet.cell(column=4, row=len(Prices)+3, value=sum(Valorisations))
+        #wb_obj._sheets.sort(key=lambda ws: ws.title)
+
+        wb_obj.save(file)
+        #Make Financial Statement Final in first position
+        fs_sheet_actual_position = wb_obj.worksheets.index(financial_statement_sheet) #get position of new sheet financial statement sheet
+        fs_sheet_new_position = 0
+        sheets = wb_obj._sheets.copy()
+        sheets.insert(fs_sheet_new_position, sheets.pop(fs_sheet_actual_position)) #modifying the sheets list
+        wb_obj._sheets = sheets
+        wb_obj.save(file)
