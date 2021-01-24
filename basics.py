a90675e6ed2d901b73ee1b2cf6b295bc1c3e9148
Nandy Bâ <nandyba@yahoo.fr>
2021-01-24 17:50:21 +0100

    test variable d'environnement clé API Coinbase partie 2
    Vérification de la valeur de la variable d'environement

--- basics.py
+++ basics.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
 try:
-	load_dotenv('.env')
-	Coinbas_api_key = os.environ["Coinbase_API_Key"]
+	Coinbase_api_key = os.environ["Coinbase_API_Key"]
+	if Coinbase_api_key == "":
+		raise Exception()
 except Exception as e:
 	raise e
 print("test passed")
